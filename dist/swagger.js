3de8d39a4d9b059453f6e9b54469c18c34b9886b
bitebit <kfbygj@gmail.com>
2016-11-16 21:21:01 +0800

    move example to top level

--- dist/swagger.js
+++ dist/swagger.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 let _ = require('lodash');
 let debug = require('debug')('koa-router-swagger:swagger');
@@ -144,9 +144,9 @@ swagger.prototype.apiExplorer = function (ctx, next) {
   self.app.use(mount('/koa-router-swagger', statics(swaggerDist)));
 
   // mount api-explorer
+  console.log(`ApiExplorer: http://127.0.0.1:${ self.port }/api-explorer`);
   return mount('/api-explorer', (() => {
     var _ref = _asyncToGenerator(function* (ctx) {
-      console.log(`ApiExplorer: http://127.0.0.1:${ self.port }/api-explorer`);
       yield ctx.render('index', { url: pathUtil.basename(self.apiDoc) });
     });
 
